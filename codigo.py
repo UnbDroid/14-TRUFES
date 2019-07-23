b7b8a5edb648a67ac7a16d8320483988e5cfd353
Caio <caioaa@droidunb.com>
2019-07-22 21:01:58 -0300

    Aproximaçoes consertadas, falta tornar mais redundante e testar

--- codigo.py
+++ codigo.py
@@ -7,13 +7,17 @@ import time
 # Definindo valores de leitura do ColorSensor
 COLOR_BLACK = 1
 COLOR_WHITE = 6
+# Definindo variaveis globais para mudar mais facil
+TAMANHO_ARENA = 7	# Numero de espacos na arena (Do zero)
+ROTACAO = 1	# Quanto as rodas precisam rodar juntas pro robo girar 90 graus
+VELOC_BASE = 20		# A porcentagem de velocidade base dos motores
 
 def acharHome(ultra, drive, color):
 	"Acha o primeiro quadrado e define a posição atual"
 	dist = ultra.value()
-	drive.on(SpeedPercent(20),SpeedPercent(20))
+	drive.on(SpeedPercent(VELOC_BASE),SpeedPercent(VELOC_BASE))
 	
-	while dist > 190: #Aqui iria o sensor de distância
+	while dist > 170: 
 		dist = ultra.value()
 		print("Procurando parede")
 		print(dist)
@@ -23,12 +27,13 @@ def acharHome(ultra, drive, color):
 	return cor
 
 def percorreArena(arena, posiAt, posiAn, ultra, motorEsq, motorDir, sensorEsq, sensorDir):
-	"Percorre a borda do grid, para achar os cubos"
+	"Percorre a borda do grid, para achar os cubos. Retorna a arena completa."
 	achar = 0 #Sensor de distância, verficar cubos
 	numBlocos = -1 #Quantos blocos foram encontrados
 	posiBlocos = [[0] * 2 for i in range(4)] #4 Vetores de 2 '0's cada
 	distMotores = 0 #Quanto o motor rotacionou
-	distAn = 2250 #Distância medida anteriormente pelo Ultrassom
+	tank_drive = MoveTank(OUTPUT_B, OUTPUT_D)
+	distUAn = 2250 #Distância medida anteriormente pelo Ultrassom
 	andandoEm = 0 #0 - X, 1 - Y
 	deveProcurar = 0
 
@@ -37,33 +42,37 @@ def percorreArena(arena, posiAt, posiAn, ultra, motorEsq, motorDir, sensorEsq, s
 		distU = ultra.value()
 		deveProcurar = 0
 
-		if (posiAt[1] == 7 or posiAt[1] == 0 or posiAt[0] == 7) and distU < 120:
+		if (posiAt[1] == TAMANHO_ARENA or posiAt[1] == 0) and distU < 150:
+			#O fim da arena sempre ocorre em X = 0 ou X = 7
 			#Chegou no fim, virar e continuar
-			tank_drive.on_for_rotations(SpeedPercent(-70),SpeedPercent(70), 0.666) #Vira pra esquerda
-			tank_drive.on_for_rotations(SpeedPercent(70),SpeedPercent(70), 2.252)  #Avança
+			while(distU > 120):
+				distU = ultra.value()
+			tank_drive.on_for_rotations(SpeedPercent(-VELOC_BASE),SpeedPercent(VELOC_BASE), ROTACAO) #Vira pra esquerda
+			tank_drive.on_for_rotations(SpeedPercent(VELOC_BASE),SpeedPercent(VELOC_BASE), 2.252)  #Avança
 
 			posiAn[0] = posiAt[0] if (posiAt[0] != posiAn[0]) else posiAt[1]
 			#Se estou andando em Y, quando virar o Y se manterá até a próxma parede
 			#Se estou andando em X, agora o Y deve aumentar ou diminuir (baseado no X atual)
 			if(posiAt[0] != posiAn[0]): posiAt[0] = posiAn[1]
 
-			posiAn[1] = posiAt[1] if (posiAt[1] != posiAn[1]) else (7 - posiAt[0])
+			posiAn[1] = posiAt[1] if (posiAt[1] != posiAn[1]) else (TAMANHO_ARENA - posiAt[0])
 			#Se estou andando em X, quando virar o X se manterá até a próxima parede
 			#Se estou andando em Y, agora o X deve aumentar ou diminuir (baseado no Y atual)
-			if(posiAt[1] != posiAn[1]): posiAt[1] = 7 - posiAn[0]
+			if(posiAt[1] != posiAn[1]): posiAt[1] = (TAMANHO_ARENA) - posiAn[0]
 
 		motorEsq.reset()
 		motorDir.reset()
 		distU = ultra.value()
-		distAn = distU
+		distUAn = distU
 		corEsq = sensorEsq.value()
 		corDir = sensorDir.value()
-		tank_drive.on(SpeedPercent(40),SpeedPercent(40))
+		tank_drive.on(SpeedPercent(VELOC_BASE),SpeedPercent(VELOC_BASE))
 
-		while(distMotores < 300 and (distAn - distU) > 300): #Anda um quadrado
+		while(distMotores < 300 or (distUAn - distU) < 300): #Anda um quadrado
 			distMotores = int((motorEsq.value() + motorDir.value())/2)
 			distU = ultra.value()
 			if(corEsq == COLOR_BLACK or corDir == COLOR_BLACK):
+				print("Checa")
 				#Funcao checa alinhamento
 			corEsq = sensorEsq.value()
 			corDir = sensorDir.value()
@@ -90,7 +99,7 @@ def percorreArena(arena, posiAt, posiAn, ultra, motorEsq, motorDir, sensorEsq, s
 			posiAt[0] += 1
 			andandoEm = 0
 
-		for i in range(7):
+		for i in range(TAMANHO_ARENA):
 			#Verifica se existe ainda algum espaço não conhecido('0') na sequência observada
 			if(andandoEm):
 				#Andando em Y, verificar em X
@@ -103,33 +112,39 @@ def percorreArena(arena, posiAt, posiAn, ultra, motorEsq, motorDir, sensorEsq, s
 				
 
 		if(deveProcurar):#Inicia o padrão
-			tank_drive.on_for_rotations(SpeedPercent(-70),SpeedPercent(70), 0.666) #Vira pra esquerda
+			tank_drive.on_for_rotations(SpeedPercent(-VELOC_BASE),SpeedPercent(VELOC_BASE), ROTACAO) #Vira pra esquerda
 
-			if(dist < 2000): #Achou algo...
+			if(distU < ((300*TAMANHO_ARENA)-100)): #Achou algo...
 				numBlocos += 1
-				#VV Verfificar se o arrendondamento funciona direito
+				#VV Verifificar se o arrendondamento funciona direito
 				if(posiAn[1] == posiAt[1]): #...enquanto andava em X
+					correc = 0 if posiAt[0] else 1 #Correcao de +1 se viu a partir de um 0
 					posiBlocos[numBlocos][1] = posiAt[1] #Está na posição X do robô
-					posiBlocos[numBlocos][0] = int(dist / 300) + 1 #Arredondamento da Distância em Y
+					posiBlocos[numBlocos][0] = abs(posiAt[0] - (int(distU / 300) + correc)) #Arredondamento da Distância em Y
 				else: #...enquanto andava em Y
+					correc = 0 if posiAt[1] else 1 #Correcao de +1 se viu a partir de um 0
 					posiBlocos[numBlocos][0] = posiAt[0] #Está na posição Y do robô
-					posiBlocos[numBlocos][1] = int(dist / 300) + 1 #Arredondamento da Distância em X
-				for i in range(4):
-					if(posiBlocos[i][0] == posiBlocos[numBlocos][0] and posiBlocos[i][1] == posiBlocos[numBlocos][1]):
-						#Achou um bloco que já tinha achado (Inútil?)
-						numBlocos -= 1
+					posiBlocos[numBlocos][1] = abs(posiAt[1] - (int(distU / 300) + correc)) #Arredondamento da Distância em X
+				if(numBlocos > 0):
+					for i in range(numBlocos):
+						print("Passo")
+						print(i)
+						print(numBlocos)
+						if(posiBlocos[i][0] == posiBlocos[numBlocos][0] and posiBlocos[i][1] == posiBlocos[numBlocos][1]):
+							#Achou um bloco que já tinha achado (Inútil?) (Atrapalha?)
+							numBlocos -= 1
 			else:
-				for i in range(7):
-					if(andandoEM):
+				for i in range(TAMANHO_ARENA):
+					if(andandoEm):
 						#Estava olhando a sequência Y
 						arena[i][posiAt[1]] = 1
 					else:
 						#Estava olhando a seuência X
 						arena[posiAt[0]][i] = 1
 
-			tank_drive.on_for_rotations(SpeedPercent(70),SpeedPercent(-70), 0.666) #Vira pra direita
+			tank_drive.on_for_rotations(SpeedPercent(VELOC_BASE),SpeedPercent(-VELOC_BASE), ROTACAO) #Vira pra direita
 
-
+	return arena
 
 def main():
 	ultraSense = UltrasonicSensor(INPUT_1)
@@ -140,76 +155,18 @@ def main():
 	motorDir = LargeMotor(OUTPUT_D)
 	tank_drive = MoveTank(OUTPUT_B, OUTPUT_D)                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                               
 
-	arena = [[0] * 8 for i in range(8)]
-	posi_at = [7,1] #y,x
-	posi_an = [7,0]
-	motoresq.reset()
-	motordir.reset()
+	arena = [[0] * (TAMANHO_ARENA+1) for i in range(TAMANHO_ARENA+1)]
+	posi_at = [TAMANHO_ARENA,1] #y,x
+	posi_an = [TAMANHO_ARENA,0]
+	motorEsq.reset()
+	motorDir.reset()
 
 	corInicial = acharHome(ultraSense, tank_drive, sensorEsq)
+	print(corInicial)
+	tank_drive.on_for_rotations(SpeedPercent(-VELOC_BASE),SpeedPercent(VELOC_BASE), ROTACAO) #Vira pra esquerda
+	tank_drive.on_for_rotations(SpeedPercent(VELOC_BASE),SpeedPercent(VELOC_BASE), 2.252)  #Avança
+	arena = percorreArena(arena, posi_at, posi_an, ultraSense, motorEsq, motorDir, sensorEsq, sensorDir)
 
 main()
-# tank_drive.on_for_rotations(SpeedPercent(40),SpeedPercent(-40), 0.666)
-# tank_drive.on_for_rotations(SpeedPercent(60),SpeedPercent(60), 0.5)
-# tank_drive.on_for_rotations(SpeedPercent(40),SpeedPercent(-40), 0.666)
-
-# while a < 1200:
-# 	a = motor1.position
-#  	b = motor2.position
-#  	andado = (a+b)/2
-#tank_drive.on_for_rotations(SpeedPercent(40),SpeedPercent(40), 2.252*8)
-# while achar != 1:
-# 		if posi[1] == 7:
-# 			tank_drive.on_for_rotations(SpeedPercent(-40),SpeedPercent(40), 0.666)
-# 			tank_drive.on_for_rotations(SpeedPercent(40),SpeedPercent(40), 2.273)
-# 			tank_drive.on_for_rotations(SpeedPercent(-40),SpeedPercent(40), 0.666)
-# 			posi[0] -= 1
-# 		elif posi[1] == 0:
-#print(corInicial)
-#tank_drive.on_for_rotations(SpeedPercent(90),SpeedPercent(90), 0.1)
-#tank_drive.on(SpeedPercent(0),SpeedPercent(0))
-#time.sleep(1)
-#tank_drive.on_for_rotations(SpeedPercent(40),SpeedPercent(40), 0.2)
-#tank_drive.on(SpeedPercent(25),SpeedPercent(0))
-#time.sleep(1)
-#tank_drive.on(SpeedPercent(0),SpeedPercent(0))
 
 tank_drive.on(SpeedPercent(0),SpeedPercent(0))
-	# delta = a - b  
-	# potr -= delta*0.005+int_delta*0.0000034
-	# potl += delta*0.005+int_delta*0.0000034
-
-	# if potr > 50:
-	# 	potr = 50
-	# 	pass
-
-	# if potl > 50:
-	# 	potl = 50
-	# 	pass
-
-	# motor1.on(SpeedPercent(potr))
-	# motor2.on(SpeedPercent(potl))
-
-		
-	# if a < (b + 18):
-	# 	while a < (b + 18):
-	# 		motor1.on(SpeedPercent(40))
-	# 		motor2.on(SpeedPercent(37))
-	# 		a = motor1.position
-	# 		b = motor2.position
-	# elif (b + 18) < a:
-	# 	while (b + 18) < a:
-	# 		motor1.on(SpeedPercent(37))
-	# 		motor2.on(SpeedPercent(40))
-	# 		a = motor1.position
-	# 		b = motor2.position
-	# else:
-	# 	motor1.on(SpeedPercent(40))
-	# 	motor2.on(SpeedPercent(40))
-#print(s)
-#tank_drive.on_for_rotations(SpeedPercent(50), SpeedPercent(50),0.1)
-#tank_drive.on(SpeedPercent(0), SpeedPercent(0))
-#tank_drive.on_for_rotations(SpeedPercent(40), SpeedPercent(40),1.67)
-#tank_drive.on_for_rotations(SpeedPercent(-40), SpeedPercent(0),1.25)
-#tank_drive.on_for_rotations(SpeedPercent(40), SpeedPercent(40), 3.7)
-
