284108da6e9938ba2072091403381df31c87a436
Waliff Cordeiro <waliff.cordeiro@gmail.com>
2019-07-26 23:11:18 -0300

    função garra.py

--- garra.py
+++ garra.py
@@ -3,44 +3,31 @@ from ev3dev2.sensor import *
 from ev3dev2.sensor.lego import *
 import time
 
-tank_drive = MoveTank(OUTPUT_B, OUTPUT_D)  
-ultrassom = UltrasonicSensor(INPUT_1)
-garra1 = Motor(OUTPUT_A)
-garra2 = Motor(OUTPUT_C)
+def pegaBloco(garra1, garra2, tank_drive):
+	garra1.on_for_rotations(SpeedPercent(10), 0.2) #garra da esquerda abre
+	garra2.on_for_rotations(SpeedPercent(-10), 0.2) #garra da direita abre
+	tank_drive.on_for_rotations(SpeedPercent(20),SpeedPercent(20), 0.5) #movimenta com base em 125mm de distância do cubo
+	garra1.on_for_rotations(SpeedPercent(-10), 0.2) #garra da esquerda fecha
+	garra2.on_for_rotations(SpeedPercent(10), 0.2) #garra da direita fecha
 
-#garra1.on_for_rotations(SpeedPercent(-10), 0.4) #garra da esquerda fecha; seconds 0.35
-#garra2.on_for_rotations(SpeedPercent(10), 0.4) #garra da direita fecha
-tank_drive.on(SpeedPercent(20),SpeedPercent(20))
+def largaBloco(garra1, garra2, tank_drive):
+	garra1.on_for_rotations(SpeedPercent(10), 0.2) #garra da esquerda abre
+	garra2.on_for_rotations(SpeedPercent(-10), 0.2) #garra da direita abre
+	tank_drive.on_for_rotations(SpeedPercent(-20),SpeedPercent(-20), 0.5) #ré com base em 125mm de distância do cubo
+	garra1.on_for_rotations(SpeedPercent(-10), 0.2) #garra da esquerda fecha
+	garra2.on_for_rotations(SpeedPercent(10), 0.2) #garra da direita fecha
 
-#while (1):
-#	teste = ultrassom.value()
-#	print("Valor")
-#	print(teste)
+if __name__ == '__main__':
+	tank_drive = MoveTank(OUTPUT_B, OUTPUT_D)  
+	ultrassom = UltrasonicSensor(INPUT_1)
+	garra1 = Motor(OUTPUT_A)
+	garra2 = Motor(OUTPUT_C)
+	
+	tank_drive.on(SpeedRPM(20), SpeedRPM(20)) #inicia movimento
+	while(ultrassom.value() > 125):
+		g=1
+	tank_drive.on(SpeedRPM(0), SpeedRPM(0)) #para movimento
+	pegaBloco(garra1, garra2, tank_drive) #função de aproximar e pegar o bloco
+	tank_drive.on_for_rotations(SpeedPercent(-100),SpeedPercent(0), 4) # 360 para teste
+	largaBloco(garra1, garra2, tank_drive) #função que afasta e larga o bloco
 
-while(ultrassom.value() > 125):
-	g=1
-
-tank_drive.on(SpeedPercent(0),SpeedPercent(0))
-	#garra.on_for_rotations(SpeedPercent(-10),SpeedPercent(10), 0.5)
-
-garra1.on_for_rotations(SpeedPercent(10), 0.2) #garra da esquerda abre
-garra2.on_for_rotations(SpeedPercent(-10), 0.2) #garra da direita abre
-#time.sleep(3)
-#garra1.on_for_rotations(SpeedPercent(-10), 0.2) #garra da esquerda fecha; seconds 0.35
-#garra2.on_for_rotations(SpeedPercent(10), 0.2) #garra da direita fecha
-
-
-tank_drive.on_for_rotations(SpeedPercent(20),SpeedPercent(20), 0.5)
-
-garra1.on_for_rotations(SpeedPercent(-10), 0.2) #garra da esquerda fecha; seconds 0.35
-garra2.on_for_rotations(SpeedPercent(10), 0.2) #garra da direita fecha
-
-tank_drive.on_for_rotations(SpeedPercent(100),SpeedPercent(0), 4)
-tank_drive.on_for_rotations(SpeedPercent(50),SpeedPercent(50), 1)
-
-garra1.on_for_rotations(SpeedPercent(10), 0.2) #garra da esquerda abre
-garra2.on_for_rotations(SpeedPercent(-10), 0.2) #garra da direita abre
-tank_drive.on_for_rotations(SpeedPercent(-50),SpeedPercent(-50), 2)
-
-garra1.on_for_rotations(SpeedPercent(-10), 0.2) #garra da esquerda fecha; seconds 0.35
-garra2.on_for_rotations(SpeedPercent(10), 0.2) #garra da direita fecha
