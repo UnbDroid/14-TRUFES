18fea659876dbd0faa61ba87aeb6c9acc2f45bb5
Waliff Cordeiro <waliff.cordeiro@gmail.com>
2019-08-25 11:32:58 -0300

    comentários support

--- source/support.py
+++ source/support.py
@@ -4,17 +4,22 @@ def filterultrassom(ultrassom):
 	valor = 0
 	for i in range(0, 5):
 		valor += ultrassom.value()
-	print("VAL:", valor/5)
 	return int(valor/5)
 
 def drift(super, move_tank, move_steering):
 	#Se super for True, o robô vai dar drift de ré.
 	#Se não, ele faz a volta na lavanderia
 	if(super):
+		# Porcentagem de graus baseado na velocidade da roda pivô
+		# Ex: Quando vale 25, a roda direita girará a metade da velocidade da roda esquera, e contrario é valido para 75.
+		# < 50 == Esquerda como pivô
+		# > 50 == Direita como pivô
+		# == 50 Continua reto
 		move_steering.on_for_rotations(22, SpeedPercent(-50), 4.45)
 		move_tank.on_for_rotations(SpeedPercent(40), SpeedPercent(40), 0.7)
 	else:
-		move_tank.on_for_rotations(SpeedPercent(40), SpeedPercent(40), 2.252)
-		move_tank.on_for_rotations(SpeedPercent(30), SpeedPercent(-30), 1.05)
-		move_tank.on_for_rotations(SpeedPercent(40), SpeedPercent(40), 2.252)
-		move_tank.on_for_rotations(SpeedPercent(-30), SpeedPercent(30), 2.05)
+		# Contorna Cubo
+		move_tank.on_for_rotations(SpeedPercent(40), SpeedPercent(40), 2.252) # Anda um quadrado
+		move_tank.on_for_rotations(SpeedPercent(30), SpeedPercent(-30), 1.05) # 90º direita
+		move_tank.on_for_rotations(SpeedPercent(40), SpeedPercent(40), 2.252) # anda um quadrado
+		move_tank.on_for_rotations(SpeedPercent(-30), SpeedPercent(30), 2.05) # 180º esquerda
